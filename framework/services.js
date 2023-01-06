955fe240a0cbace1ed14cce1f826e87848087a58
Sashka <aitmn08@gmail.com>
2023-01-06 16:54:07 +0500

    add test framework, controller +  refactoring of tests

---
+++ framework/services.js
@@ -0,0 +1,38 @@
+import config from "./config";
+import supertest from 'supertest';
+const {url} = config;
+
+
+const user = {
+    login: (payload) => {
+        return supertest(url)
+        .post('/api/login')
+        .send(payload)
+    },
+    reg: (id) => {
+        return supertest(url)
+        .post('/api/register')
+        .send(id)
+    },
+    resource: () => {
+        return supertest(url)
+        .get('/api/{resource}/2')
+        .send()
+    },
+    create: (newUser) => {
+        return supertest(url)
+        .post('/api/users')
+        .send(newUser)
+    },
+    list: (usersList) => {
+        return supertest(url)
+        .get('/api/users?page=1')
+        .send(usersList)
+    }
+}
+
+
+
+
+export default user
+
--- specs/api.spec.js
+++ specs/api.spec.js
@@ -1,110 +1,57 @@
 import axios from 'axios';
+import config from '../framework/config'
+import user from '../framework/services'
+import supertest from 'supertest';
 
 describe('testing https://reqres.in/api', () => {
 
     test('Get request, list users', async () => {
-
-        const config = {
-            method: 'get',
-            url: 'https://reqres.in/api/users?page=1',
-            body: {
-            }
-        }
-        const resp = await axios(config)
-        //console.log(resp.data)
+        const resp = await user.list()
+        console.log(resp.status, resp._body.page)
         expect(resp.status).toEqual(200)
-        expect(resp.data.page).toEqual(1)
+        expect(resp._body.page).toEqual(1)
     })
 
     test('Post request, create new user', async () => {
-        const config = {
-            method: 'post',
-            url: 'https://reqres.in/api/users',
-            body: {
-                name: 'Sasha',
-                job: 'QA'
-            }
-        }
-
-        const resp = await axios(config)
-        //console.log(resp.config)
+        const resp = await user.create(config.body)
+        console.log(resp.status, resp._body)
         expect(resp.status).toEqual(201)
+        
     })
     test('Get request, fetches an unknown resource', async () =>{
-        const config = {
-            method: 'get',
-            url: 'https://reqres.in/api/{resource}/2',
-            data: {}
-        }
-        const resp = await axios(config)
-        //console.log(resp.data.data)
+        const resp = await user.resource()
+        console.log(resp._body.data.id, resp.status)
         expect(resp.status).toEqual(200)
-        expect(resp.data.data.id).toEqual(2)
+        expect(resp._body.data.id).toEqual(2)
     })
 
     test('Post request, registration succesfull', async () =>{
-        const config = {
-            method: 'post',
-            url: 'https://reqres.in/api/register',
-            data: {
-                email: 'eve.holt@reqres.in',
-                password: 'pistol'
-            }
-        }
-        const resp = await axios(config)
-        //console.log(resp)
+        const resp = await user.reg(config.data)
+        console.log(resp.status, resp._body)
         expect(resp.status).toEqual(200)
-        expect(resp.data).toEqual({ id: 4, token: 'QpwL5tke4Pnpja7X4' })
+        expect(resp._body).toEqual({ id: 4, token: 'QpwL5tke4Pnpja7X4' })
     })
 
     test('Post request, login succesfull', async () =>{
-        const config = {
-            method: 'post',
-            url: 'https://reqres.in/api/login',
-            data: {
-                email: 'eve.holt@reqres.in',
-                password: 'cityslicka'
-            }
-        }
-        const resp = await axios(config)
-        //console.log(resp)
+        const resp = await user.login(config.data)
+        console.log(resp._body, resp.status)
         expect(resp.status).toEqual(200)
-        expect(resp.data.token).toEqual('QpwL5tke4Pnpja7X4')
+        expect(resp._body.token).toEqual('QpwL5tke4Pnpja7X4')
     })
 
     test('Post request, register without password', async() =>{
-        const config = {
-            method: 'post',
-            url: 'https://reqres.in/api/register',
-            data: {
-                email: 'eve.holt@reqres.in',
-            }
-        }
-        try {
-        const resp = await axios(config)
-        }
-        catch(e){
-        //console.log(e.response)
-        expect(e.response.status).toEqual(400)
-        expect(e.response.data).toEqual({ error: 'Missing password' })
-        }
+        const resp = await user.login({email: 'eve.holt@reqres.in'})
+        console.log(resp._body, resp.status)
+        expect(resp.status).toEqual(400)
+        expect(resp._body).toEqual({ error: 'Missing password' })
+        
     })
 
     test('Post request, register without email', async() =>{
-        const config = {
-            method: 'post',
-            url: 'https://reqres.in/api/register',
-            data: {
-                password: 'cityslicka'
-            }
-        }
-        try {
-        const resp = await axios(config)
-        }
-        catch(e){
-        //console.log(e.response)
-        expect(e.response.status).toEqual(400)
-        expect(e.response.data).toEqual({ error: 'Missing email or username' })
-        }
+        const resp = await user.login({password: 'pistol'})
+        console.log(resp._body, resp.status)
+        expect(resp.status).toEqual(400)
+        expect(resp._body).toEqual({ error: 'Missing email or username' })
+        
     })
 })

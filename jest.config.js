77ca71dd36ff4f7f93f0b77a2b2b1bd2b905c1d3
Sashka <aitmn08@gmail.com>
2023-02-05 18:36:22 +0500

    add test timeout

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    testTimeout: 30000,
     verbose: true,
     testMatch: [ "**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)" ],
     transform: {"\\.[jt]sx?$": "babel-jest",},
